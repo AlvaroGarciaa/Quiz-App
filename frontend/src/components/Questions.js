30c9f843bc5c86b36ba939196ca1aad8c4d98181
JosueBVN <A01751694@tec.mx>
2023-05-17 20:26:51 -0600

    Part 2 Questions part
    i implemented the questions part, but there is no connection page to page yet

--- frontend/src/components/Questions.js
+++ frontend/src/components/Questions.js
@@ -1,111 +1,90 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-function QuestionPage({ question, answers, onNextQuestion }) {
+import QuizTimer from './QuizTimer';
+import QuizScoreboard from './QuizScoreboard';
+
+import '../styles/QuizPage.css'; // Import the CSS file for styling
+
+const QuizPage = () => {
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [isAnswered, setIsAnswered] = useState(false);
+  const [isCorrect, setIsCorrect] = useState(false);
   const [score, setScore] = useState(0);
-  const [timer, setTimer] = useState(30);
 
-  useEffect(() => {
-    const countdown = setInterval(() => {
-      setTimer((prevTimer) => prevTimer - 1);
-    }, 1000);
+  const questions = [
+    {
+      title: 'Question 1',
+      options: [
+        { title: 'Option A', isCorrect: true },
+        { title: 'Option B', isCorrect: false },
+        { title: 'Option C', isCorrect: false },
+        { title: 'Option D', isCorrect: false },
+      ],
+    },
+    // Add more questions here...
+  ];
 
-    if (timer === 0) {
-      clearInterval(countdown);
-      handleNextQuestion();
+  const handleOptionClick = (isCorrect) => {
+    setIsAnswered(true);
+    setIsCorrect(isCorrect);
+    if (isCorrect) {
+      const timeRemaining = 30; // Get the remaining time from QuizTimer component
+      setScore(score + (timeRemaining * 25));
     }
-
-    return () => clearInterval(countdown);
-  }, [timer]);
-
-  const handleAnswer = (selectedAnswer) => {
-    if (selectedAnswer.correct) {
-      setScore((prevScore) => prevScore + 1);
-    }
-
-    handleNextQuestion();
   };
 
   const handleNextQuestion = () => {
-    setTimer(30);
-    onNextQuestion();
+    setQuestionIndex(questionIndex + 1);
+    setIsAnswered(false);
+    setIsCorrect(false);
   };
 
+  const currentQuestion = questions[questionIndex];
+
   return (
-    <div
-      style={{
-        background: 'black',
-        height: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
-      }}
-    >
-      <div
-        style={{
-          background: 'white',
-          padding: '20px',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
+    <div className="quiz-page">
+      <QuizTimer
+        isAnswered={isAnswered}
+        onTimeout={handleNextQuestion}
+      />
+
+      <Button className="exit-button" variant="contained" color="primary">
+        Exit
+      </Button>
+
+      <Box className={`white-box ${isAnswered && isCorrect ? 'green' : isAnswered ? 'red' : ''}`}>
+        <Typography variant="h5" component="h2" className="question-title">
+          {currentQuestion.title}
+        </Typography>
+
+        {currentQuestion.options.map((option, index) => (
+          <Button
+            key={index}
+            className={`option-button ${isAnswered && option.isCorrect ? 'correct' : ''}`}
+            variant="outlined"
+            onClick={() => handleOptionClick(option.isCorrect)}
+          >
+            {option.title}
+          </Button>
+        ))}
+      </Box>
+
+      <QuizScoreboard score={score} />
+
+      <Button
+        className="next-button"
+        variant="contained"
+        color="primary"
+        disabled={!isAnswered}
+        onClick={handleNextQuestion}
       >
-        <div
-          style={{
-            width: '60px',
-            height: '60px',
-            borderRadius: '50%',
-            background: 'gray',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginBottom: '20px',
-          }}
-        >
-          <span style={{ fontSize: '24px', color: 'white' }}>{timer}</span>
-        </div>
-        <h2 style={{ textAlign: 'center' }}>{question}</h2>
-        <div
-          style={{
-            display: 'grid',
-            gridTemplateColumns: 'repeat(2, 1fr)',
-            gridGap: '10px',
-            marginBottom: '20px',
-          }}
-        >
-          {answers.map((answer, index) => (
-            <button
-              key={index}
-              style={{
-                background: 'white',
-                border: 'none',
-                padding: '10px',
-                borderRadius: '5px',
-                fontSize: '16px',
-              }}
-              onClick={() => handleAnswer(answer)}
-            >
-              {answer.text}
-            </button>
-          ))}
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            width: '100%',
-            marginBottom: '20px',
-          }}
-        >
-          <div style={{ background: 'gray', padding: '10px', borderRadius: '5px' }}>
-            Score: {score}
-          </div>
-          <button onClick={handleNextQuestion}>Next Question</button>
-        </div>
-      </div>
+        Next Question
+      </Button>
     </div>
   );
-}
+};
 
-export default QuestionPage;
-
+export default QuizPage;
