3f29d64c9ab15ed8a0db2833aedfe0a23b2443b7
m3llad0 <a01655451@tec.mx>
2023-05-21 18:53:54 -0600

    added new containers and error states

--- frontend/src/components/PatternProPage.js
+++ frontend/src/components/PatternProPage.js
@@ -1,55 +1,95 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import Question from "./Questions.js";
+import {useNavigate} from 'react-router';
+import { TextField, Button, StyledEngineProvider, Alert } from "@mui/material";
 import "../styles/PatternProPage.css"; // Import the CSS file
 
 function PatternProPage() {
+  const navigate = useNavigate();
   const [name, setName] = useState("");
   const [numQuestions, setNumQuestions] = useState("");
+  const [errors, setErrors] = useState({
+    name: false,
+    questions: false,
+    start: false
+  })
 
   const handleNameChange = (e) => {
-    setName(e.target.value);
+    const v = e.target.value
+    if(!/^[0-9a-zA-Z]{1,8}$/.test(v))
+      setErrors({...errors, name: true})
+    else
+      setErrors({...errors, name: false})
+      setName(v);
   };
 
   const handleNumQuestionsChange = (e) => {
-    setNumQuestions(e.target.value);
-  };
+    const v = e.target.value
 
-  const handleNextPage = () => {
-    ReactDOM.render(<Question />, document.getElementById("root"));
+    if(!/^(?:[1-9]|10)$/.test(v))
+      setErrors({...errors, questions: true})
+    else
+    setErrors({...errors, questions: false})
+    setNumQuestions(v);
   };
 
   return (
+   <StyledEngineProvider>
     <div className="page-container">
-      {" "}
-      {/* Use class name */}
-      <div className="content-container">
-        {" "}
         {/* Use class name */}
-        <h1 className="page-title">PatternPro</h1> {/* Use class name */}
-        <input
-          class="input-container"
-          type="text"
-          value={name}
-          placeholder="Name"
-          required={true}
-          onChange={handleNameChange}
-        />
-        <div>
-          <input
-            class="input-container"
+        <div className="content-container">
+          {/* Use class name */}
+          <h1 className="page-title">PatternPro</h1> {/* Use class name */}
+          <div className="input-container">
+            {errors.start ? (
+              <Alert severity="error" variant="filled">Wrong information</Alert>
+            ) : null}
+            <TextField
+            error = {errors.name}
+            fullWidth
+            variant="outlined"
+            label="Name"
+            onChange={handleNameChange}/>
+
+            <TextField
+            error = {errors.questions}
+            fullWidth
             type="number"
-            value={numQuestions}
+            variant="outlined"
+            label="Number of questions"
             onChange={handleNumQuestionsChange}
-            placeholder="Number of Questions"
-            required={true}
-          />
+            sx={{
+              fontFamily: 'Inter',
+              fontWeight: "700"
+            }}/> 
+            </div> 
         </div>
-      </div>
-      <button className="next-button" onClick={handleNextPage}>
-        Start
-      </button>
+        <div className="button-container">
+          <Button
+          onClick={() => {
+            if(errors.name && errors.questions)
+              setErrors({...errors, start: true})
+            else
+              navigate("/quiz ")
+          }}
+          variant="contained"
+          className="start-button"
+          sx={{
+            background: "#6137E3",
+            textTransform: "none",
+            paddingTop: "0.5rem",
+            paddingBottom: "0.5rem",
+            fontSize: "2.5rem",
+            fontFamily: "Inter",
+            fontWeight: 700,
+            borderRadius: "0.9rem",
+            width: "15%"
+
+          }}
+          disableElevation
+          >Start</Button>
+        </div>
     </div>
+    </StyledEngineProvider>
   );
 }
 
