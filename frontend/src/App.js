fc62f291c51f0e563da8fa2f9ec10fb425316107
m3llad0 <a01655451@tec.mx>
2023-05-21 18:53:09 -0600

    added routes using react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
+import {Routes, Route} from 'react-router-dom';
 import PatternProPage from "./components/PatternProPage.js";
+import QuizPage from "./components/Questions.js";
+
 function App() {
   return (
-    <div>
-    <PatternProPage/>      
-    </div>
+    <Routes>
+      <Route path="/" element={<PatternProPage/>}/>
+      <Route path="/quiz" element={<QuizPage/>}/>
+    </Routes>
   );
 }
 
