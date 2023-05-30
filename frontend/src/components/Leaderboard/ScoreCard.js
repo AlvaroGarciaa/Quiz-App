e5177bc36263c1460bc956aa0189495cc7562aef
m3llad0 <a01655451@tec.mx>
2023-05-30 00:44:51 -0600

    scorecard component

---
+++ frontend/src/components/Leaderboard/ScoreCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import '../../styles/Scorecard.css'
+
+
+const ScoreCard = () => {
+
+    return(
+        <div className="scorecard-container">
+            <div className="place">
+                1
+            </div>
+            <div className="user">
+                    Juan
+            </div>
+            <div className="user-score">
+                    1000
+            </div>
+        </div>
+    )
+}
+
+export default ScoreCard;
