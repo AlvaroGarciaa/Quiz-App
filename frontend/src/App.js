33950e043fb0ca4f72182d279173a4d81b7d1962
m3llad0 <a01655451@tec.mx>
2023-05-30 00:45:31 -0600

    added leaderboard page to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import {Routes, Route} from 'react-router-dom';
 import PatternProPage from "./pages/PatternProPage.js";
 import QuizPage from "./pages/Questions.js";
 import EndQuiz from './pages/EndQuiz.js';
+import Leaderboard from './pages/Leaderboard.js';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<PatternProPage/>}/>
       <Route path="/quiz" element={<QuizPage/>}/>
       <Route path="/finish" element = {<EndQuiz/>}/>
+      <Route path="/leaderboard" element = {<Leaderboard/>}/>
     </Routes>
   );
 }
