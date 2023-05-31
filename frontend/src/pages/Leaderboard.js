91fc9a07c6bb5b997ab3fa19f9454ca5aebea684
m3llad0 <a01655451@tec.mx>
2023-05-30 19:02:51 -0600

    get data from api

--- frontend/src/pages/Leaderboard.js
+++ frontend/src/pages/Leaderboard.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "@mui/material";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ScoreCard from "../components/Leaderboard/ScoreCard";
 import '../styles/Leaderboard.css';
 
 const Leaderboard = () => {
+  const API = process.env.REACT_APP_API;
+  const [score, setScore] = useState([]);
 
-    return(
-        <div className="page">
-           <div className="return-container">
-            <Button
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${API}/getTop10`);
+        if (response.ok) {
+          const data = await response.json();
+          const slicedData = data.slice(0, 5);
+          setScore(slicedData);
+        } else {
+          throw new Error('Error en la respuesta de la petición GET');
+        }
+      } catch (error) {
+        console.error('Error al obtener los datos:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="page">
+      <div className="return-container">
+        <Button
           sx={{
             textTransform: "none",
             color: "#A3A4AA",
@@ -17,20 +38,24 @@ const Leaderboard = () => {
             fontFamily: "Inter",
             fontSize: "1rem",
           }}
-          startIcon={<ArrowBackIosIcon/>}
-            >Return</Button>
-            </div>
-          <div className="container">
-            <h1>Leaderboard</h1>
-            <ScoreCard/>
-            <ScoreCard/>
-            <ScoreCard/>
-            <ScoreCard/>
-            <ScoreCard/>
-          </div> 
-            
-        </div>
-    )
+          startIcon={<ArrowBackIosIcon />}
+        >
+          Return
+        </Button>
+      </div>
+      <div className="leaderboard-container">
+        <h1>Leaderboard</h1>
+        {score.map((item, index) => (
+          <ScoreCard
+            key={index}
+            place={index + 1}
+            name={item.name}
+            score={item.score}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Leaderboard;
